2fc982258300711ba9d42d7a3c5d14fc11b83779
zhuchentong <2037630@163.com>
2026-04-25 10:51:50 +0800

    feat: add eslint and trouble plugins with diagnostic keymaps

--- lua/community.lua
+++ lua/community.lua
@@ -9,8 +9,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.media.image-nvim" },
-  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.pack.vue" },                 -- vue支持
+  { import = "astrocommunity.media.image-nvim" },         -- 图片显示支持
+  { import = "astrocommunity.motion.mini-move" },         -- 单/多行移动支持
+  { import = "astrocommunity.pack.eslint" },              -- eslint支持
+  { import = "astrocommunity.diagnostics.trouble-nvim" }, -- 诊断列表优化支持
+
 }
--- lua/plugins/user.keymaps.lua
+++ lua/plugins/user.keymaps.lua
@@ -8,6 +8,10 @@ return {
         ["K"] = { "5k", desc = "Move up 5 lines" }, -- 向上移动5行
         ["H"] = { "^", desc = "Move to line start" }, -- 移动到行首
         ["L"] = { "$", desc = "Move to line end" }, -- 移动到行末
+        ["<F8>"] = { function() vim.diagnostic.goto_next() end, desc = "Next diagnostic" },
+        ["<S-F8>"] = { function() vim.diagnostic.goto_prev() end, desc = "Previous diagnostic" },
+        ["<C-m>"] = { function() vim.lsp.buf.code_action() end, desc = "Code Action" },
+        ["<C-A-m>"] = { "<Cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Document Diagnostics" },
       },
       v = {
         ["J"] = { "5j", desc = "Move down 5 lines" }, -- 向下移动5行
