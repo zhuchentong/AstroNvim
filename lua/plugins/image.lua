31361bcebb53c23ce1051a51552e6aec70d1f171
zhuchentong <2037630@163.com>
2026-04-27 21:15:59 +0800

    fixed: update image.lua

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -2,7 +2,10 @@ return {
   "3rd/image.nvim",
   opts = function()
     local opts = {}
-    if vim.env.TERM:match "^foot" then opts.backend = "sixel" end
+
+    -- 设置图像渲染后端
+    if ({ "foot", "foot-extra" })[vim.env.TERM] then opts.backend = "sixel" end
+
     return opts
   end,
 }
--- lua/user/functions.lua
+++ lua/user/functions.lua
@@ -3,4 +3,15 @@ local M = {}
 --- keymaps 相关函数
 M.keymaps = {}
 
+M.list = {}
+
+function M.list.contains(list, value)
+    for _, v in ipairs(list) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
+
 return M
